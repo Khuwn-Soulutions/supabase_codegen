8314db90537e0e6d9c018e1e2c3a51404c4d318f
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-02-26 17:07:05 -0500

    test(mocks): add supabase client mock

--- test/src/supabase/mocks/mocks.dart
+++ test/src/supabase/mocks/mocks.dart
@@ -1,1 +1,2 @@
+export 'supabase_client.mock.dart';
 export 'user_table_row_mock.dart';
---
+++ test/src/supabase/mocks/supabase_client.mock.dart
@@ -0,0 +1,5 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:supabase/supabase.dart';
+
+/// Mock [SupabaseClient]
+class MockSupabaseClient extends Mock implements SupabaseClient {}
