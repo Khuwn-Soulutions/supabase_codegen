4312a86fa32bea335c840610d815922178f25e19
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-02-25 23:42:08 -0500

    test: write inital test to check when env file not found

---
+++ test/supabase_codegen.dart
@@ -0,0 +1,10 @@
+import 'package:supabase_codegen/supabase_codegen.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('generateSupabaseTypes', () {
+    test('it throws an error when env file not found', () {
+      expect(generateSupabaseTypes(envFilePath: '.no-env'), throwsException);
+    });
+  });
+}
