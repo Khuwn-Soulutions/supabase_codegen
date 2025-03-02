35c95fff9f59b8d597e1545d6d153bc046d83ab4
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-03-02 01:55:50 -0500

    test(database): add sql to create tables and seed data for users and test_generate

---
+++ test/sql/seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO "public"."test_generate" ("id", "created_at", "is_nullable", "is_array", "is_not_nullable") VALUES ('12131f30-cb53-43ee-b2d5-cc1c8e52111c', '2025-03-02 05:40:01.2145+00', 'is_nullable', '"{\"array\"]}', 'not_nullable');
+INSERT INTO "public"."users" ("email", "acc_name", "phone_number", "contacts", "role", "created_at") VALUES ('john@example.com', 'John Doe', '+1234567890', '"{\"me@them.com\"]}', 'admin', '2025-03-02 05:38:09.432015+00');
