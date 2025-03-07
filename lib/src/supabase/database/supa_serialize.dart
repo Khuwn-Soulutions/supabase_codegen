eb6ade72881b351f13ada2216d8eec03517a4b88
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-03-07 01:13:14 -0500

    feat(database): move serialization and deserialization utilities to supa_serialize.dart

---
+++ lib/src/supabase/database/supa_serialize.dart
@@ -0,0 +1,81 @@
+import 'dart:convert';
+import 'package:collection/collection.dart';
+import 'package:latlng/latlng.dart';
+import 'package:meta/meta.dart';
+import 'package:supabase_codegen/supabase_codegen.dart';
+
+/// Serialize the [value] provided
+@protected
+dynamic supaSerialize<T>(T? value) {
+  /// Handle null value
+  if (value == null) {
+    return null;
+  }
+
+  switch (value) {
+    case DateTime _:
+      return (value as DateTime).toIso8601String();
+    case PostgresTime _:
+      return (value as PostgresTime).toIso8601String();
+    case LatLng _:
+      final latLng = value as LatLng;
+      return {'lat': latLng.latitude, 'lng': latLng.longitude};
+    case final Enum enumValue:
+      return enumValue.name;
+    default:
+      return value;
+  }
+}
+
+/// Serialize a list
+@protected
+List<T>? supaSerializeList<T>(List<T>? value) {
+  final values = value?.map((v) => supaSerialize<T>(v));
+  return values == null ? null : List<T>.from(values);
+}
+
+/// Deserialize a value
+@protected
+T? supaDeserialize<T>(dynamic value, {List<T> enumValues = const []}) {
+  /// Handle null value
+  if (value == null) {
+    return null;
+  }
+
+  /// Handle enum deserialization
+  if (enumValues.isNotEmpty) {
+    return (enumValues as List<Enum>)
+        .firstWhereOrNull((val) => val.name == value) as T?;
+  }
+
+  /// Handle other types
+  switch (T) {
+    case const (int):
+      return (value as num).round() as T?;
+    case const (double):
+      return (value as num).toDouble() as T?;
+    case const (DateTime):
+      return DateTime.tryParse(value as String)?.toLocal() as T?;
+    case const (PostgresTime):
+      return PostgresTime.tryParse(value as String) as T?;
+    case const (LatLng):
+      final latLng = value is Map ? value : json.decode(value as String) as Map;
+      final lat = latLng['lat'] ?? latLng['latitude'];
+      final lng = latLng['lng'] ?? latLng['longitude'];
+      return lat is num && lng is num
+          ? LatLng.degree(lat.toDouble(), lng.toDouble()) as T?
+          : null;
+    default:
+      return value as T;
+  }
+}
+
+/// Deserialize a list
+@protected
+List<T>? supaDeserializeList<T>(dynamic value) => value is List
+    ? value
+        .map((v) => supaDeserialize<T>(v))
+        .where((v) => v != null)
+        .map((v) => v as T)
+        .toList()
+    : null;
--- lib/src/supabase/database/supabase_data_row.dart
+++ lib/src/supabase/database/supabase_data_row.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:collection/collection.dart';
-import 'package:latlng/latlng.dart';
 import 'package:meta/meta.dart';
 import 'package:supabase_codegen/supabase_codegen.dart';
 
@@ -26,7 +23,7 @@ abstract class SupabaseDataRow {
     T? defaultValue,
     List<T> enumValues = const [],
   }) =>
-      _supaDeserialize<T>(data[fieldName], enumValues: enumValues) ??
+      supaDeserialize<T>(data[fieldName], enumValues: enumValues) ??
       defaultValue;
 
   /// Set the value of a field in the [data]
@@ -36,7 +33,7 @@ abstract class SupabaseDataRow {
 
   /// Get a field within the [data] as a List
   List<T> getListField<T>(String fieldName) =>
-      _supaDeserializeList<T>(data[fieldName]) ?? [];
+      supaDeserializeList<T>(data[fieldName]) ?? [];
 
   /// Set the List [value] of the [fieldName] within [data]
   void setListField<T>(String fieldName, List<T>? value) =>
@@ -63,77 +60,4 @@ ${data.entries.map(
   bool operator ==(Object other) =>
       other is SupabaseDataRow &&
       const DeepCollectionEquality().equals(other.data, data);
-
-  /// Serialize the [value] provided
-  dynamic supaSerialize<T>(T? value) {
-    /// Handle null value
-    if (value == null) {
-      return null;
-    }
-
-    switch (value) {
-      case DateTime _:
-        return (value as DateTime).toIso8601String();
-      case PostgresTime _:
-        return (value as PostgresTime).toIso8601String();
-      case LatLng _:
-        final latLng = value as LatLng;
-        return {'lat': latLng.latitude, 'lng': latLng.longitude};
-      case final Enum enumValue:
-        return enumValue.name;
-      default:
-        return value;
-    }
-  }
-
-  /// Serialize a list
-  List<T>? supaSerializeList<T>(List<T>? value) {
-    final values = value?.map((v) => supaSerialize<T>(v));
-    return values == null ? null : List<T>.from(values);
-  }
-
-  /// Deserialize a value
-  T? _supaDeserialize<T>(dynamic value, {List<T> enumValues = const []}) {
-    /// Handle null value
-    if (value == null) {
-      return null;
-    }
-
-    /// Handle enum deserialization
-    if (enumValues.isNotEmpty) {
-      return (enumValues as List<Enum>)
-          .firstWhereOrNull((val) => val.name == value) as T?;
-    }
-
-    /// Handle other types
-    switch (T) {
-      case const (int):
-        return (value as num).round() as T?;
-      case const (double):
-        return (value as num).toDouble() as T?;
-      case const (DateTime):
-        return DateTime.tryParse(value as String)?.toLocal() as T?;
-      case const (PostgresTime):
-        return PostgresTime.tryParse(value as String) as T?;
-      case const (LatLng):
-        final latLng =
-            value is Map ? value : json.decode(value as String) as Map;
-        final lat = latLng['lat'] ?? latLng['latitude'];
-        final lng = latLng['lng'] ?? latLng['longitude'];
-        return lat is num && lng is num
-            ? LatLng.degree(lat.toDouble(), lng.toDouble()) as T?
-            : null;
-      default:
-        return value as T;
-    }
-  }
-
-  /// Deserialize a list
-  List<T>? _supaDeserializeList<T>(dynamic value) => value is List
-      ? value
-          .map((v) => _supaDeserialize<T>(v))
-          .where((v) => v != null)
-          .map((v) => v as T)
-          .toList()
-      : null;
 }
