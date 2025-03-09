9227fdc5c92d5f894f7f694abb30bbbf96ac7356
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-03-08 22:17:47 -0500

    feat(logger): move logger implementation to file

---
+++ bin/src/logger.dart
@@ -0,0 +1,4 @@
+import 'package:logger/logger.dart';
+
+/// Logger
+late final Logger logger;
--- bin/src/src.dart
+++ bin/src/src.dart
@@ -1,8 +1,4 @@
-import 'package:logger/logger.dart';
-
 export 'generate_supabase_types.dart';
 export 'generators/generators.dart';
 export 'get_types.dart';
-
-/// Logger
-late final Logger logger;
+export 'logger.dart';
