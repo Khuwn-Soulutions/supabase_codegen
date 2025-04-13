b469cc20b7182eb78cedaa19ba74cb6f9ab93345
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-04-12 23:52:44 -0400

    feat(users):  implement user update functionality

--- example/lib/users/users_detail_screen.dart
+++ example/lib/users/users_detail_screen.dart
@@ -1,11 +1,22 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:supabase_codegen_example/supabase_codegen_example.dart';
 import 'package:supabase_codegen_example/types/database.dart';
 
-class UsersDetailScreen extends StatelessWidget {
+class UsersDetailScreen extends StatefulWidget {
   final UsersRow user;
 
   const UsersDetailScreen({required this.user, super.key});
+
+  @override
+  State<UsersDetailScreen> createState() => _UsersDetailScreenState();
+}
+
+class _UsersDetailScreenState extends State<UsersDetailScreen> {
+  late UsersRow _user = widget.user;
+  final UsersTable _usersTable = UsersTable();
 
   // Helper to display optional fields
   Widget _buildDetailRow(String label, String? value) {
@@ -39,14 +50,28 @@ class UsersDetailScreen extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
         // Show user's name or email in the title
         title: Text(
-          user.accName?.isNotEmpty == true ? user.accName! : user.email,
+          _user.accName?.isNotEmpty == true ? _user.accName! : _user.email,
         ),
         // Edit/Delete actions
         actions: [
           IconButton(
             icon: const Icon(Icons.edit),
-            onPressed: () {
-              /* Navigate to edit */
+            onPressed: () async {
+              await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => UsersEditScreen(user: _user),
+                ),
+              );
+              final update = await _usersTable.querySingleRow(
+                queryFn: (q) => q.eq(UsersRow.idField, _user.id),
+              );
+              if (update == null) {
+                log('Error on fetching updated user');
+                return;
+              }
+
+              setState(() => _user = update);
             },
           ),
           IconButton(
@@ -61,18 +86,18 @@ class UsersDetailScreen extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: ListView(
           children: [
-            _buildDetailRow('ID', user.id),
-            _buildDetailRow('Email', user.email),
-            _buildDetailRow('Account Name', user.accName),
-            _buildDetailRow('Phone Number', user.phoneNumber),
-            _buildDetailRow('Role', user.role.name),
-            _buildDetailRow('Created At', _formatDate(user.createdAt)),
+            _buildDetailRow('ID', _user.id),
+            _buildDetailRow('Email', _user.email),
+            _buildDetailRow('Account Name', _user.accName),
+            _buildDetailRow('Phone Number', _user.phoneNumber),
+            _buildDetailRow('Role', _user.role.name),
+            _buildDetailRow('Created At', _formatDate(_user.createdAt)),
             const SizedBox(height: 10),
             const Text(
               'Contacts:',
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            if (user.contacts.isEmpty)
+            if (_user.contacts.isEmpty)
               const Padding(
                 padding: EdgeInsets.only(top: 4.0),
                 child: Text('N/A'),
@@ -82,7 +107,7 @@ class UsersDetailScreen extends StatelessWidget {
                 padding: const EdgeInsets.only(top: 4.0),
                 child: Column(
                   children:
-                      user.contacts.map((contact) {
+                      _user.contacts.map((contact) {
                         return ListTile(
                           leading: const Icon(Icons.person),
                           title: Text(contact),
