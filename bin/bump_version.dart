48a27039081efcb886b80435ed74e14451003d97
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-04-13 16:00:41 -0400

    fix(bump_version): remove version update logic for footer in readme

--- bin/bump_version.dart
+++ bin/bump_version.dart
@@ -40,15 +40,10 @@ void main(List<String> args) async {
   final readmeFile = File('README.md');
   final readmeContents = readmeFile.readAsStringSync();
   readmeFile.writeAsStringSync(
-    readmeContents
-        .replaceAll(
-          RegExp(r'supabase_codegen: \^(.+)'),
-          'supabase_codegen: ^$version',
-        )
-        .replaceAll(
-          RegExp(r'supabase_codegen \((.+)\)'),
-          'supabase_codegen ($version)',
-        ),
+    readmeContents.replaceAll(
+      RegExp(r'supabase_codegen: \^(.+)'),
+      'supabase_codegen: ^$version',
+    ),
   );
   logger.i('README.md updated');
 }
