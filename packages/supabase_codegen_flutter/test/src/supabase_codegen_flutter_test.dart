4a602250abafb7881e5d42b20f982a99996b5187
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-04-17 13:28:21 -0400

    fix: use const constructor for SupabaseCodegenFlutter instantiation in tests

--- packages/supabase_codegen_flutter/test/src/supabase_codegen_flutter_test.dart
+++ packages/supabase_codegen_flutter/test/src/supabase_codegen_flutter_test.dart
@@ -1,12 +1,10 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:supabase_codegen_flutter/supabase_codegen_flutter.dart';
 
 void main() {
   group('SupabaseCodegenFlutter', () {
     test('can be instantiated', () {
-      expect(SupabaseCodegenFlutter(), isNotNull);
+      expect(const SupabaseCodegenFlutter(), isNotNull);
     });
   });
 }
