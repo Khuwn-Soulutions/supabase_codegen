c3712d3e1d7e0881fa1938656ed64a9a64feba35
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-04-17 16:49:14 -0400

    refactor(supabase_codegen_flutter): loadMockSupabaseClient method to throw UnimplementedError

--- packages/supabase_codegen_flutter/lib/src/supabase_codegen_flutter_client.dart
+++ packages/supabase_codegen_flutter/lib/src/supabase_codegen_flutter_client.dart
@@ -88,15 +88,6 @@ class SupabaseCodegenFlutterClient implements SupabaseCodegenClientBase {
   @override
   @visibleForTesting
   Future<SupabaseClient> loadMockSupabaseClient() async {
-    await initSupabase(
-      url: 'url',
-      key: 'key',
-      // Used within testing only
-      // ignore: invalid_use_of_visible_for_testing_member
-      httpClient: mockSupabaseHttpClient,
-    );
-    // Hide warning as the method is marked visible for testing
-    // ignore: invalid_use_of_visible_for_testing_member
-    return setClient(Supabase.instance.client = mockSupabase);
+    throw UnimplementedError('Use MockSupabaseCodegenClient instead');
   }
 }
