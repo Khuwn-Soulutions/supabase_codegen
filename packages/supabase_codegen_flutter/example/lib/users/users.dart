4c78a8fd53ec815322d838c2727eac7cd9bedb29
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-04-17 16:02:09 -0400

    feat(supabase_codegen_flutter): add example project

---
+++ packages/supabase_codegen_flutter/example/lib/users/users.dart
@@ -0,0 +1,3 @@
+export 'users_detail_screen.dart';
+export 'users_edit_screen.dart';
+export 'users_master_screen.dart';
---
+++ packages/supabase_codegen_flutter/example/test/users_master_screen_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:supabase_codegen_flutter/supabase_codegen_flutter.dart';
+
+import 'package:supabase_codegen_example/supabase_codegen_example.dart';
+import 'package:supabase_codegen_example/types/tables/users.dart';
+
+void main() {
+  group('Users Master Screen ', () {
+    MockSupabaseCodegenClient mockClient;
+
+    setUpAll(() async {
+      mockClient = MockSupabaseCodegenClient();
+      await mockClient.loadMockSupabaseClient();
+    });
+
+    testWidgets('displays all users', (WidgetTester tester) async {
+      await UsersTable().insertRow(
+        UsersRow(email: 'me@example.com', accName: 'John Doe'),
+      );
+      // Build the screen
+      await tester.pumpWidget(MaterialApp(home: const UsersMasterScreen()));
+      await tester.pumpAndSettle();
+
+      // Verify that user name and email are displayed
+      expect(find.text('John Doe'), findsOneWidget);
+      expect(find.text('me@example.com'), findsOneWidget);
+    });
+  });
+}
