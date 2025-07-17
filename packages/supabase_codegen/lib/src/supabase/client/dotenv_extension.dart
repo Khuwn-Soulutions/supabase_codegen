f6ccc2d000cd78a0507242a5fc1ea02da6629be5
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-07-17 04:56:32 -0400

    feat(supabase_codegen): read supabase secrets from platform.env

--- packages/supabase_codegen/lib/src/supabase/client/dotenv_extension.dart
+++ packages/supabase_codegen/lib/src/supabase/client/dotenv_extension.dart
@@ -1,13 +1,47 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:dotenv/dotenv.dart';
 import 'package:supabase_codegen/src/supabase/client/supabase_env_keys.dart';
 
 /// Dotenv extension
 extension DotenvExtension on DotEnv {
   /// Extract the required keys from the environment file at [envPath]
-  ({String supabaseUrl, String supabaseKey}) extractKeys(String envPath) {
+  ({String supabaseUrl, String supabaseKey}) extractKeys(
+    String envPath, [
+    Map<String, String>? env,
+  ]) {
+    return extractFromPlatformEnv() ?? extractFromPath(envPath);
+  }
+
+  /// Extract the keys from the platform environment
+  ({String supabaseKey, String supabaseUrl})? extractFromPlatformEnv([
+    Map<String, String>? env,
+  ]) {
+    final environment = env ?? Platform.environment;
+    final hasUrl = environment.containsKey(supabaseEnvKeys.url);
+    if (!hasUrl) {
+      return null;
+    }
+    final supabaseKey = environment[supabaseEnvKeys.anonKey] ??
+        environment[supabaseEnvKeys.key] ??
+        '';
+    if (supabaseKey.isEmpty) {
+      return null;
+    }
+
+    return (
+      supabaseUrl: environment[supabaseEnvKeys.url]!,
+      supabaseKey: supabaseKey,
+    );
+  }
+
+  /// Extract the required keys from the [envPath]
+  ({String supabaseKey, String supabaseUrl}) extractFromPath(String envPath) {
     load([envPath]);
     final hasUrl = isEveryDefined([supabaseEnvKeys.url]);
     if (!hasUrl) {
+      log('Unable to find env: ${File.fromUri(Uri.file(envPath)).path}');
       throw Exception(
         '[GenerateTypes] Missing ${supabaseEnvKeys.url} in $envPath file. ',
       );
