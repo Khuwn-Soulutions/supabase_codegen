45f0a3d5b26dafa6cd807b6ded30f14be5885c51
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-08-05 13:29:39 -0400

    feat(supabase_codegen_flutter): export client instances as static fields

--- packages/supabase_codegen_flutter/lib/src/supabase_codegen_flutter.dart
+++ packages/supabase_codegen_flutter/lib/src/supabase_codegen_flutter.dart
@@ -6,4 +6,19 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class SupabaseCodegenFlutter {
   /// Supabase instance
   static Supabase get supabase => Supabase.instance;
+
+  /// Client instance
+  static SupabaseClient get client => supabase.client;
+
+  /// Auth client instance
+  static GoTrueClient get auth => client.auth;
+
+  /// Realtime client instance
+  static RealtimeClient get realtime => client.realtime;
+
+  /// Storage client instance
+  static SupabaseStorageClient get storage => client.storage;
+
+  /// Functions client instance
+  static FunctionsClient get functions => client.functions;
 }
