1eb7ba7329cf3218d46e65a7cde5223e98b70084
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-10-02 00:00:32 -0400

    feat(supabase_codegen): add schema overrides

--- packages/supabase_codegen/lib/src/generator/generate_types/generate_types.dart
+++ packages/supabase_codegen/lib/src/generator/generate_types/generate_types.dart
@@ -2,3 +2,4 @@ export 'cmd_option.dart';
 export 'default_values.dart';
 export 'get_config.dart';
 export 'run_generate_types.dart';
+export 'schema_overrides/schema_overrides.dart';
---
+++ packages/supabase_codegen/lib/src/generator/generate_types/schema_overrides/schema_overrides.dart
@@ -0,0 +1,3 @@
+export 'column_override.dart';
+export 'override_types.dart';
+export 'parse_override.dart';
