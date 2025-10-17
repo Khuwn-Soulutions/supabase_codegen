fb1ed8e410bf9a2718d800b2de215e5d57730672
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-10-17 08:50:56 -0400

    feat(supabase_codegen_serverpod): add initial implementation of Supabase Codegen Serverpod library

---
+++ packages/supabase_codegen_serverpod/lib/src/supabase_codegen_serverpod.dart
@@ -0,0 +1,8 @@
+/// {@template supabase_codegen_serverpod}
+/// A codegen library that generates models for serverpod
+/// from your Supabase tables and enums
+/// {@endtemplate}
+class SupabaseCodegenServerpod {
+  /// {@macro supabase_codegen_serverpod}
+  const SupabaseCodegenServerpod();
+}
---
+++ packages/supabase_codegen_serverpod/lib/supabase_codegen_serverpod.dart
@@ -0,0 +1,5 @@
+/// A codegen library that generates models for serverpod
+/// from your Supabase tables and enums
+library;
+
+export 'src/supabase_codegen_serverpod.dart';
