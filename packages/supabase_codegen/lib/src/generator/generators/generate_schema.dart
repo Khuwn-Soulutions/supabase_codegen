fc31e114df6deef1714ea0e5fdea18de38e9ed83
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-10-20 15:23:58 -0400

    refactor(supabase_codegen): update schema generation folder referencing for testability

--- packages/supabase_codegen/lib/src/generator/generators/generate_schema.dart
+++ packages/supabase_codegen/lib/src/generator/generators/generate_schema.dart
@@ -5,8 +5,10 @@ import 'package:supabase_codegen/src/generator/generator.dart';
 // coverage:ignore-file
 
 /// Generate schema files
-Future<void> generateSchemaFiles() async {
-  final enumsDir = Directory('$root/enums');
+Future<void> generateSchemaFiles([
+  Directory? enumsDir,
+]) async {
+  enumsDir ??= enumsDirectory;
 
   // Generate enums file
   await generateEnums(enumsDir);
