c5024b704f2263286905cf6f9b410a605a33cf9e
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-10-27 16:46:02 -0400

    feat(core): serialize and deserialize uuid

--- packages/supabase_codegen/lib/src/supabase/database/supa_serialize.dart
+++ packages/supabase_codegen/lib/src/supabase/database/supa_serialize.dart
@@ -2,8 +2,10 @@ import 'dart:convert';
 import 'package:collection/collection.dart';
 import 'package:latlng/latlng.dart';
 import 'package:meta/meta.dart';
+import 'package:uuid/uuid_value.dart';
 
-/// Serialize the [value] provided
+/// Serialize the [value] provided from [value]
+/// with type [T] to json value
 @protected
 dynamic supaSerialize<T>(T? value) {
   /// Handle null value
@@ -17,6 +19,8 @@ dynamic supaSerialize<T>(T? value) {
     case LatLng _:
       final latLng = value as LatLng;
       return {'lat': latLng.latitude.degrees, 'lng': latLng.longitude.degrees};
+    case UuidValue _:
+      return (value as UuidValue).uuid;
     case final Enum enumValue:
       return enumValue.name;
     default:
@@ -31,7 +35,7 @@ List<dynamic>? supaSerializeList<T>(List<T>? value) {
   return values == null ? null : List<dynamic>.from(values);
 }
 
-/// Deserialize a value
+/// Deserialize a value from json [value] to [T]
 @protected
 T? supaDeserialize<T>(dynamic value, {List<T> enumValues = const []}) {
   /// Handle null value
@@ -60,6 +64,8 @@ T? supaDeserialize<T>(dynamic value, {List<T> enumValues = const []}) {
       return lat is num && lng is num
           ? LatLng.degree(lat.toDouble(), lng.toDouble()) as T?
           : null;
+    case const (UuidValue):
+      return UuidValue.withValidation(value as String) as T?;
     default:
       return value as T;
   }
