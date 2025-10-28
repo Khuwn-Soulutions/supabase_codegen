acf5756405216dfb6557ccfdc07af1d4b8a3bf6a
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-10-28 07:08:30 -0400

    refactor(core): rename dart_type file to is_dynamic_extension

--- packages/supabase_codegen/lib/src/generator/generate_types/generate_types.dart
+++ packages/supabase_codegen/lib/src/generator/generate_types/generate_types.dart
@@ -3,5 +3,6 @@ export 'dart_type.dart';
 export 'default_values.dart';
 export 'field_name_type_map.dart';
 export 'get_config.dart';
+export 'is_dynamic_extension.dart';
 export 'run_generate_types.dart';
 export 'schema_overrides/schema_overrides.dart';
