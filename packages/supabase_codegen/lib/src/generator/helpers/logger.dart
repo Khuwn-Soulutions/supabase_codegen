6c36388bb97b37c719a11f5284a03eb87237d591
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-11-15 19:24:19 -0500

    refactor(core): initialize logger

--- packages/supabase_codegen/lib/src/generator/helpers/logger.dart
+++ packages/supabase_codegen/lib/src/generator/helpers/logger.dart
@@ -2,7 +2,7 @@ import 'package:mason_logger/mason_logger.dart';
 import 'package:meta/meta.dart';
 
 /// A simple logger using mason_logger package.
-late Logger logger;
+Logger logger = Logger();
 
 /// Test logger instance
 @visibleForTesting
