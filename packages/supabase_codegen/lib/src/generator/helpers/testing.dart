9145037471dee2f105ae4a803397feefc7de3c27
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-11-16 02:38:40 -0500

    chore(core): ignore testing helper

--- packages/supabase_codegen/lib/src/generator/helpers/testing.dart
+++ packages/supabase_codegen/lib/src/generator/helpers/testing.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 /// Are we running in test mode
+// coverage:ignore-file
 final isRunningInTest =
     Platform.script.path.contains('test.dart') ||
     Platform.environment['FLUTTER_TEST'] == 'true';
