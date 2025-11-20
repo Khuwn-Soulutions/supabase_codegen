a512b9e3ff8dad8d98e3267d74d5ebfef77f45ff
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-11-20 16:54:25 -0500

    feat(serverpod): spy bundle generator

---
+++ packages/supabase_codegen_serverpod/lib/src/spy_bundle_generator.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:mason/mason.dart';
+import 'package:supabase_codegen/supabase_codegen_generator.dart';
+import 'package:supabase_codegen_serverpod/supabase_codegen_serverpod.dart';
+
+/// Bundle Generator for Serverpod Yaml Files
+class SpyBundleGenerator extends BundleGenerator {
+  /// Constructor
+  const SpyBundleGenerator();
+
+  /// Generated files
+  static List<GeneratedFile> generatedFiles = [];
+
+  @override
+  Future<void> generateFiles(
+    Directory outputDir,
+    GeneratorConfig upserts, [
+    GeneratorConfig? config,
+  ]) async {
+    final progress = logger.progress('Generating Spy Files...');
+    await generateSpyFiles(
+      outputDir,
+      upserts.copyWith(package: 'supabase_codegen_serverpod'),
+    );
+    progress.complete();
+  }
+
+  /// Generate the spy.yaml files for the models provided
+  Future<void> generateSpyFiles(
+    Directory outputDir,
+    GeneratorConfig config,
+  ) async {
+    final generator = await MasonGenerator.fromBundle(serverpodBundle);
+    final target = DirectoryGeneratorTarget(outputDir);
+    final serverpodJson = config.toServerpodJson();
+    logger.detail('Serverpod Json: $serverpodJson');
+    final files = await generator.generate(
+      target,
+      vars: serverpodJson,
+    );
+    generatedFiles.addAll(files);
+  }
+}
--- packages/supabase_codegen_serverpod/lib/src/src.dart
+++ packages/supabase_codegen_serverpod/lib/src/src.dart
@@ -1,3 +1,6 @@
-export 'codegen_utils.dart';
+export 'bundles/bundles.dart';
+export 'extensions/extensions.dart';
+export 'helpers/helpers.dart';
 export 'supabase_code_generator.dart';
+export 'spy_bundle_generator.dart';
 export 'supabase_codegen_serverpod.dart';
