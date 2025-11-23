af18b5578ca7515ce2d98d6a7af3a90cb06e7e6d
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-11-22 23:28:36 -0500

    feat(serverpod): include `JsonClass` in Serverpod's `generator.yaml` on initialization

--- packages/supabase_codegen_serverpod/bin/init.dart
+++ packages/supabase_codegen_serverpod/bin/init.dart
@@ -1,9 +1,13 @@
 import 'package:supabase_codegen/init/init.dart';
+import 'package:supabase_codegen_serverpod/supabase_codegen_serverpod.dart';
 
 /// Default output path for the generated code
 const defaultOutputPath = 'lib/src/models';
 
 /// Call the supabase_codegen init with relevant environment
-void main() => init(
-  defaultOutputPath: defaultOutputPath,
-);
+Future<void> main() async {
+  await init(
+    defaultOutputPath: defaultOutputPath,
+  );
+  await addExtraClassesToGeneratorConfig();
+}
