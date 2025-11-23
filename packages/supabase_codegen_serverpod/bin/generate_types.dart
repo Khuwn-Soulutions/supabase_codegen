2dc7f710f13c9bf550415bec446e2ed6dfcb160b
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-11-22 19:27:04 -0500

    refactor: use `GeneratorConfigParams` in `runGenerateTypes` for configuration

--- packages/supabase_codegen_serverpod/bin/generate_types.dart
+++ packages/supabase_codegen_serverpod/bin/generate_types.dart
@@ -1,11 +1,15 @@
 import 'package:supabase_codegen/supabase_codegen_generator.dart';
-import 'package:supabase_codegen_serverpod/src/src.dart';
+import 'package:supabase_codegen_serverpod/supabase_codegen_serverpod.dart';
+import 'init.dart';
 
 /// Main function
 void main(List<String> args) async {
   await runGenerateTypes(
     args,
     generator: const ServerpodCodeGenerator(),
-    package: 'supabase_codegen_serverpod',
+    config: GeneratorConfigParams.empty().copyWith(
+      package: 'supabase_codegen_serverpod',
+      outputFolder: defaultOutputPath,
+    ),
   );
 }
