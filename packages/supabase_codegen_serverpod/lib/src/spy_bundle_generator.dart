d6066b3adc23e7ac7e65519a5dc095225be31bec
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-12-15 17:50:06 -0500

    feat(serverpod): return generated files list from bundle generator

--- packages/supabase_codegen_serverpod/lib/src/spy_bundle_generator.dart
+++ packages/supabase_codegen_serverpod/lib/src/spy_bundle_generator.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:mason/mason.dart';
-import 'package:meta/meta.dart';
 import 'package:supabase_codegen/supabase_codegen_generator.dart';
 import 'package:supabase_codegen_serverpod/supabase_codegen_serverpod.dart';
 
@@ -10,41 +9,42 @@ class SpyBundleGenerator extends BundleGenerator {
   /// Constructor
   const SpyBundleGenerator();
 
-  /// Generated files
-  @visibleForTesting
-  static List<GeneratedFile> generatedFiles = [];
-
   @override
-  Future<void> generateFiles(
+  Future<List<GeneratedFile>> generateFiles(
     Directory outputDir,
     GeneratorConfig? upserts, [
     // Unused: Serverpod generates yaml files, not barrel files
     GeneratorConfig? _,
     List<GeneratedFile>? generated,
   ]) async {
+    final generatedFiles = generated ?? [];
     final progress = logger.progress('Generating Spy Files...');
     if (upserts == null) {
       progress.fail('No upserts config provided. Skipping Spy file generation');
-      return;
+      return generatedFiles;
     }
 
     try {
-      await generateSpyFiles(outputDir, upserts);
-      await generateRpcFunctions(outputDir, upserts);
+      final spyFiles = await generateSpyFiles(outputDir, upserts);
+      generatedFiles.addAll(spyFiles);
+      final rpcFiles = await generateRpcFunctions(outputDir, upserts);
+      generatedFiles.addAll(rpcFiles);
 
       // Run post generation clean up process
-      await cleanup(outputDir);
+      await cleanup(outputDir, generatedFiles: generatedFiles);
       progress.complete();
     }
     // Catch all exceptions and errors
-    // ignore: avoid_catches_without_on_clauses
     catch (e) {
       progress.fail('Generation failed: $e');
+      rethrow;
     }
+
+    return generatedFiles;
   }
 
   /// Generate the spy.yaml files for the models provided
-  Future<void> generateSpyFiles(
+  Future<List<GeneratedFile>> generateSpyFiles(
     Directory outputDir,
     GeneratorConfig config,
   ) async {
@@ -52,10 +52,9 @@ class SpyBundleGenerator extends BundleGenerator {
     final target = DirectoryGeneratorTarget(outputDir);
     final serverpodJson = config.toServerpodJson();
     logger.detail('Serverpod Json: $serverpodJson');
-    final files = await generator.generate(
+    return generator.generate(
       target,
       vars: serverpodJson,
     );
-    generatedFiles.addAll(files);
   }
 }
