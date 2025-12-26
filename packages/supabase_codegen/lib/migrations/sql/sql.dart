3a6fac5118acd21cb480ce600c59a9f034daadb3
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-12-26 07:43:06 -0500

    fix(core): use package imports

--- packages/supabase_codegen/lib/migrations/sql/sql.dart
+++ packages/supabase_codegen/lib/migrations/sql/sql.dart
@@ -1,6 +1,6 @@
-import 'get_enum_types.dart';
-import 'get_rpc_functions.dart';
-import 'get_schema_info.dart';
+import 'package:supabase_codegen/migrations/sql/get_enum_types.dart';
+import 'package:supabase_codegen/migrations/sql/get_rpc_functions.dart';
+import 'package:supabase_codegen/migrations/sql/get_schema_info.dart';
 
 export 'get_enum_types.dart';
 export 'get_rpc_functions.dart';
